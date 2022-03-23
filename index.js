aeed79c4154817c2a13c6be3766a509731e9021d
kudzanai murefu <kudzmurefu@gmail.com>
2022-03-23 07:53:56 -0400

    1.3

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("initial line cas-3097")
-console.log("initial line cas-3097")
+console.log("initial line cas-3097") 
+console.log({OS: ["Windows","Mac OS"]})  
+console.log("initial line cas-3097") 
+alert("initial line cas-3097")  
+console.log("initial line cas-3097") 
+
+/*
+ * End Of File: cas-3027
+ */ 
