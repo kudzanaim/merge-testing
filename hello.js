57bfed7134bf08ed860df73635d820622993a118
kudzanai murefu <kudzmurefu@gmail.com>
2022-03-23 04:55:42 -0400

    2nd commit

--- hello.js
+++ hello.js
@@ -1,9 +1,5 @@
 var hello = 'Hello World'
+ 
+console.log({hello, date: Date.now()})  
 
-<<<<<<< HEAD
-console.log({hello, date: Date.now()})
-=======
-console.log({hello})
-
-var state = {appName: "helloWorld"}
->>>>>>> 13b8ebe1e9310692588ac5487d3b6afffca69ad0
+var state = {appName: "helloWorld"} 
