b0409d02a4d2b69e7de534a6689981ae61793677
kudzanai murefu <kudzmurefu@gmail.com>
2022-03-23 05:02:57 -0400

    4th commit

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ var hello = 'Hello World'
  
 console.log({hello, date: Date.now()})  
 
-var state = {appName: "helloWorld"} 
+var state = {appName: "helloWorld", a:1,b:2}
 
-var appState = {version:"1.00}
+var appState = {version:"1.00"}
