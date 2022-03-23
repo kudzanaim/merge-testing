22d8c8ab395901b244513067e0a7f74e12395de9
kudzanaim <31393676+kudzanaim@users.noreply.github.com>
2022-03-23 05:30:43 -0400

    9th commit

--- hello.js
+++ hello.js
@@ -11,3 +11,5 @@ var initLog = "System Starting...."
 var author = {name: "Isaac Asimov", releases: 43, channels:["Kindle","Spotify"]} 
 
 var system = {launched: "03-07-2006", OS: "Windows XP", needsUpdate: true}
+
+var ISO = ['x37940-90','x90040-47993x','x903683-6373']
