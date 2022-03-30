08ee9f0eb6e2637f43b77783893723152fe4b851
kudzanai murefu <kudzmurefu@gmail.com>
2022-03-30 07:52:05 -0400

    added init_app function

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ console.log("initial line cas-3097")
 var state = {version: '1.0.5', OS: 'Windows XP'}
 var state__afterCasKudzPush = {version:'1.0.1', date: Date.now()}
 var state_other_dev = {OS:'IOS',status: 'new'}
+
+const init_app = function(){
+    return {state}
+}
