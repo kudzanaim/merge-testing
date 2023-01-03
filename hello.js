847019c9a6eaefaef26be382e0b7b947c131382d
kudzanai murefu <kudzmurefu@gmail.com>
2023-01-03 12:27:15 -0500

    changes2

--- hello.js
+++ hello.js
@@ -16,7 +16,9 @@ var system = {launched: "03-07-2006", OS: "Windows XP", needsUpdate: true}
 
 var ISO = ['x37940-90','x90040-47993x','x903683-6373'] 
 
-// heres change #1
+// here's changes #1
+
+// heres changes #2
 
 /*
     End of File 
